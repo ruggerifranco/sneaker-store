721761a393ccd403d6a7608dba783e6172838b72
Franco Ruggeri <franco.ruigeri@andromedalatam.com>
2024-09-05 12:52:42 -0300

    url

--- app/shop/page.js
+++ app/shop/page.js
@@ -1,7 +1,7 @@
 import ProductList from '@/app/components/ProductList';
 
 const Home = async () => {
-    const res = await fetch('http://localhost:3000/api/productos', {
+    const res = await fetch('https://sneaker-store-1wk7x572d-francos-projects-29f92f5e.vercel.app/api/productos', {
         cache: 'no-store',  
     });
     const products = await res.json();
